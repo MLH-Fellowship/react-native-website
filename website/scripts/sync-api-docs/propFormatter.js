04f2ae5f59d87808fbb12f1c110bfa7363b66dba
Ayush Jain <ayushjainrksh@gmail.com>
2020-07-22 22:47:45 +0530

    Fix type of props

--- website/scripts/sync-api-docs/propFormatter.js
+++ website/scripts/sync-api-docs/propFormatter.js
@@ -89,21 +89,32 @@ function stringToInlineCodeForTable(str) {
   if (useHtml) {
     return '<code>' + he.encode(str).replace(/\|/g, '&#124;') + '</code>';
   }
-  return '`' + str + '`';
+  return str;
 }
 
 function maybeLinkifyType(flowType) {
   let url, text;
-  if (Object.hasOwnProperty.call(magic.linkableTypeAliases, flowType.name)) {
-    ({url, text} = magic.linkableTypeAliases[flowType.name]);
-  }
+  flowType.elements?.forEach(elem => {
+    if (Object.hasOwnProperty.call(magic.linkableTypeAliases, elem.name)) {
+      ({url, text} = magic.linkableTypeAliases[elem.name]);
+    }
+  });
   if (!text) {
-    text = stringToInlineCodeForTable(flowType.raw || flowType.name);
+    text = stringToInlineCodeForTable(
+      flowType.raw || formatType(flowType.name)
+    );
   }
   if (url) {
     return `[${text}](${url})`;
   }
   return text;
+}
+
+function formatType(name) {
+  if (name.toLowerCase() === 'boolean') return 'bool';
+  if (name.toLowerCase() === 'stringish') return 'string';
+  if (name === '$ReadOnlyArray') return 'array';
+  return name;
 }
 
 function maybeLinkifyTypeName(name) {
